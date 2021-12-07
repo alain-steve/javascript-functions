ddff30a45690f59e4fe6ea90c418cefa8fc6b0b9
alain-steve <alainstevemuhirwa@yahoo.com>
2021-12-07 14:03:49 +0200

    initial push

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,6 +1,10 @@
-function seed() {}
+function seed() {
+    return arguments;
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {}
